7213cefca858a7188d1e66a5a061656fda24f7aa
Nguyen Van Vu <nguyenvu20042019@gmail.com>
2023-02-08 15:06:52 +0700

    up baithi java1

---
+++ src/baithi/BookDemo.java
@@ -0,0 +1,9 @@
+package baithi;
+
+public class BookDemo {
+    public static void main(String[] args) {
+        Author author = new Author("Russel", "Winder");
+        Book book = new Book("Developing Java Software", author, 79.75);
+        System.out.println(book);
+    }
+}
